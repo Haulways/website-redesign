7a13dd685db54e2832dcef340af421e8bff92b88
Nnaemekac <nnaemekachimezie5@gmail.com>
2025-02-10 02:46:55 -0800

    First push

--- src/components/Ecosystem.jsx
+++ src/components/Ecosystem.jsx
@@ -8,7 +8,7 @@ const Ecosystem = (props) => {
         <div className='xui-px-1 xui-py-2 xui-pos-relative xui-bdr-rad-1'>
             <img src={image} alt="" className='xui-w-fluid-100' />
             <h3 className='xui-font-sz-120 xui-font-w-500'>{header}</h3>
-            <div className='xui-mt-1 xui-d-flex'>
+            <div className='xui-mt-1 xui-d-flex xui-flex-ai-center'>
                 <div className='xui-pr-1' style={{width: 'calc(100% - 40px)'}}>
                     <p className='xui-font-sz-95 xui-lg-font'>{text}</p>
                 </div>
--- src/components/home/EcosystemOverview.jsx
+++ src/components/home/EcosystemOverview.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import HaulwayOverview from "../../assets/images/home/haulway-overview.png";
+import BegorahOverview from "../../assets/images/home/begorah-overview.png";
+import GrascopeOverview from "../../assets/images/home/grascope-overview.png";
+import SunflowerOverview from "../../assets/images/home/sunflower-overview.png";
+import SunflowerAppOverview from "../../assets/images/home/sunflower-app-overview.png";
+import AmanduOverview from "../../assets/images/home/amandu-overview.png";
 import Ecosystem from '../Ecosystem';
 
 const EcosystemOverview = () => {
@@ -14,12 +19,12 @@ const EcosystemOverview = () => {
                 </div>
             </div>
             <div className='xui-mt-2 xui-d-grid xui-lg-grid-col-3 xui-grid-col-1 xui-grid-gap-3'>
-                <Ecosystem header='Haulway App' text='Social-Commerce - Mobile App Development' image={HaulwayOverview} />
-                <Ecosystem header='Haulway App' text='Social-Commerce - Mobile App Development' image={HaulwayOverview} />
-                <Ecosystem header='Haulway App' text='Social-Commerce - Mobile App Development' image={HaulwayOverview} />
-                <Ecosystem header='Haulway App' text='Social-Commerce - Mobile App Development' image={HaulwayOverview} />
-                <Ecosystem header='Haulway App' text='Social-Commerce - Mobile App Development' image={HaulwayOverview} />
-                <Ecosystem header='Haulway App' text='Social-Commerce - Mobile App Development' image={HaulwayOverview} />
+                <Ecosystem header='Haulway App' text='Social - Commerce - Mobile App Development' image={HaulwayOverview} />
+                <Ecosystem header='Begorah LTD' text='EduTech - Website Development' image={BegorahOverview} />
+                <Ecosystem header='Grascope BPO' text='Business Process Outsourcing platform - Website Development' image={GrascopeOverview} />
+                <Ecosystem header='Sunflower Pantry' text='E-commerce - Website Development' image={SunflowerOverview} />
+                <Ecosystem header='Sunflower Pantry' text='Social-Commerce - Mobile App Development' image={SunflowerAppOverview} />
+                <Ecosystem header='Amandu Farms' text='AgroTech - Website Development' image={AmanduOverview} />
             </div>
         </section>
     );
